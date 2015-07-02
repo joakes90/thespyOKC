57311f8fc05f4b793b389a7a370f38f4dcf19c4b
Justin Oakes <justin@justinoak.es>
2015-07-01 22:03:43 -0600

    added avoid session code to this branch and continued using watchOS 1.x app

--- The Spy fm/NowPlayingViewController.swift
+++ The Spy fm/NowPlayingViewController.swift
@@ -13,33 +13,39 @@ import AVFoundation
 class NowPlayingViewController: UIViewController {
     
     
-    @IBOutlet var player: UIWebView!
-    
     let songURL = NSURL(string: "http://www.thespyfm.com/media/template.html")
     var showAlert: Bool = true
     
+    @IBOutlet var pausePlayButton: UIButton!
     @IBOutlet weak var artist: UILabel!
     @IBOutlet weak var track: UILabel!
+    let Commandcenter: MPRemoteCommandCenter = MPRemoteCommandCenter.sharedCommandCenter()
+    
+    var audioPlayer: AVPlayer?
+    
+    var playing: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "togglePause", name: "pauseplay", object: nil)
+        Commandcenter.playCommand.addTarget(self, action: "toggle:")
+        Commandcenter.pauseCommand.addTarget(self, action: "toggle:")
         
-        let contentUrl: NSURL = NSURL(string: "https://oklasoftware.com/thespy/audiotest.html")!
-        let urlRequest: NSURLRequest = NSURLRequest(URL: contentUrl)
-        player.allowsInlineMediaPlayback = true
-        player.mediaPlaybackRequiresUserAction = false
-        player.scrollView.scrollEnabled = false
-        player.scrollView.bounces = false
-        player.loadRequest(urlRequest)
+        
+        audioPlayer =  AVPlayer(URL: NSURL(string: "http://50.7.70.58:8691/live")!)
+        audioPlayer?.play()
+        
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "toggle:", name: "pauseplay", object: nil)
+        
         
         let audioSession: AVAudioSession = AVAudioSession.sharedInstance()
-        audioSession.setCategory(AVAudioSessionCategoryPlayback, error: nil)
-        audioSession.setActive(true, error: nil)
-        getSongInfo()
         
-        let timer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: "getSongInfo", userInfo: nil, repeats: true)
+        audioSession.setCategory(AVAudioSessionCategoryPlayback, error: nil)
+
+        self.getSongInfo()
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: "getSongInfo", userInfo: nil, repeats: true)
         
         
     }
@@ -47,7 +53,7 @@ class NowPlayingViewController: UIViewController {
     deinit{
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -66,7 +72,9 @@ class NowPlayingViewController: UIViewController {
             "<h3>Just<br>Missed</h3>",
             "\n", "\r", "\t"]
         
-        var pageContent = NSString(contentsOfURL: songURL!, encoding: NSUTF8StringEncoding, error: nil)
+        var pageContent: NSString?
+
+        pageContent =  NSString(contentsOfURL: songURL!, encoding: NSUTF8StringEncoding, error: nil)
         
         
         for i in elementsToStrip{
@@ -77,43 +85,53 @@ class NowPlayingViewController: UIViewController {
         let infoArray = pageContent?.componentsSeparatedByString("\n")
         if infoArray == nil {
             if showAlert == true{
-                let alert: UIAlertView = UIAlertView(title: "No Network Connection", message: "The Spy FM is currently not available. Please try again later", delegate: self, cancelButtonTitle: "Ok")
-                alert.show()
+                let alert: UIAlertController = UIAlertController(title: "No Network Connection", message: "The Spy FM is currently not available. Please try again later", preferredStyle: UIAlertControllerStyle.Alert)
+                let action: UIAlertAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                    
+                })
+                
+                alert.addAction(action)
+                
+                self.presentViewController(alert, animated: true, completion: { () -> Void in
+                    
+                })
+                
                 showAlert = false
             }
         }else {
-        
-        let currentArtist: String = String(infoArray![0] as! NSString)
-        
-        let currentTrack: String = String(infoArray![19] as! NSString)
-        
-        let album: NSString = "The Spy" as NSString
-        
-        let mediaPlayer: MPNowPlayingInfoCenter = MPNowPlayingInfoCenter.defaultCenter()
-        
-        if (currentTrack != "") {
-            var rate: Float?
-            if (self.player.request?.URL?.absoluteString == "https://oklasoftware.com/thespy/playing.html" && mediaPlayer.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] as! Float == 1.0) {
-                rate = 1.0
-            } else {
-                rate = 0.0
-                self.player.loadRequest(NSURLRequest(URL: NSURL(string: "https://oklasoftware.com/thespy/audiotest.html")!))
+            
+            let currentArtist: String = String(infoArray![0] as! NSString)
+            
+            let currentTrack: String = String(infoArray![19] as! NSString)
+            
+            let album: NSString = "The Spy" as NSString
+            
+            let mediaPlayer: MPNowPlayingInfoCenter = MPNowPlayingInfoCenter.defaultCenter()
+            
+            
+            if (currentTrack != "") {
+                let rate: Double = playing == true ? 1.0 : 0.0
+                let songInfo = [MPMediaItemPropertyTitle : currentTrack, MPMediaItemPropertyArtist : currentArtist, MPMediaItemPropertyAlbumTitle : album, MPNowPlayingInfoPropertyPlaybackRate : rate]
+                mediaPlayer.nowPlayingInfo = songInfo as [String : AnyObject]
             }
+            artist.text = "By: \(currentArtist)"
+            track.text = "Now Playing: \(currentTrack)"
+        }
         
-            var songInfo = [MPMediaItemPropertyTitle : currentTrack, MPMediaItemPropertyArtist : currentArtist, MPMediaItemPropertyAlbumTitle : album, MPNowPlayingInfoPropertyPlaybackRate: rate!]
-            mediaPlayer.nowPlayingInfo = songInfo as [NSObject : AnyObject]
-            }
-        artist.text = "By: \(currentArtist)"
-        track.text = "Now Playing: \(currentTrack)"
     }
-
- }
-    func togglePause() {
-        if self.player.request?.URL?.absoluteString == "https://oklasoftware.com/thespy/playing.html" {
-            self.player.loadRequest(NSURLRequest(URL: NSURL(string: "https://oklasoftware.com/thespy/audiotest.html")!))
-        } else {
-            self.player.loadRequest(NSURLRequest(URL: NSURL(string: "https://oklasoftware.com/thespy/playing.html")!))
+    
+    
+    @IBAction func toggle(sender: AnyObject) {
+        if playing {
+            audioPlayer?.pause()
+            self.pausePlayButton.setImage(UIImage(named: "play"), forState: UIControlState.Normal)
+            playing = false
+            self.getSongInfo()
+        } else{
+            audioPlayer?.play()
+            playing = true
+            self.pausePlayButton.setImage(UIImage(named: "pause"), forState: UIControlState.Normal)
+            self.getSongInfo()
         }
     }
-
 }
